af81beb4f9a89a6fc2ff0dccc7239051b5e56007
fullstackmachina <misscyborgtech@gmail.com>
2026-04-10 09:09:12 -0400

    handle errors

--- app.js
+++ app.js
@@ -18,10 +18,17 @@ app.get("/about", (req, res) => {
 });
 
 /* GET PROJECTS PAGE */
-app.get("/projects/:id", (req, res) => {
+app.get("/projects/:id", (req, res, next) => {
   const projectId = req.params.id;
   const project = projects.find(({ id }) => id === +projectId);
-  res.render("project", { project });
+  if (project) {
+    res.render("project", { project });
+  } else {
+    const err = new Error();
+    err.status = 404;
+    err.message = "Project not found.";
+    next(err);
+  }
 });
 
 /* GET FUTURE PROJECTS PAGE */
@@ -29,6 +36,34 @@ app.get("/future_projects", (req, res) => {
   res.render("future_projects");
 });
 
+/* GET RELEASES PAGE */
+app.get("/releases", (req, res) => {
+  res.render("releases");
+});
+
+/* ERRORS */
+app.use(function handleNotFound(req, res, next) {
+  const err = new Error();
+  err.status = 404;
+  err.message = "Page not found";
+  console.log(err.message, err.status);
+  next(err);
+});
+
+app.use(function handleGlobalError(err, req, res, next) {
+  if (!err.status) {
+    err.status = 500;
+  }
+  if (!err.message) {
+    err.message = "Oops! Something went wrong!";
+  }
+
+  console.log(err.status, err.message);
+  res.status(err.status);
+  res.send(err.message);
+});
+
+// Turn on Express server
 app.listen(3000, () => {
   console.log("The application is running on localhost:3000!");
 });
